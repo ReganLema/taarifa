6838c3308d45e465aa92f21c176e569df491899e
ReganLema <reganlema44@gmail.com>
2026-06-20 10:45:28 +0300

    add healthcheck

--- taarifa_backend/taarifa_backend/urls.py
+++ taarifa_backend/taarifa_backend/urls.py
@@ -1,12 +1,17 @@
 
 # taarifa_backend/urls.py
 
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+def health(request):
+    return JsonResponse({"status": "healthy"})
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("health/", health),
+    path("admin/", admin.site.urls),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
